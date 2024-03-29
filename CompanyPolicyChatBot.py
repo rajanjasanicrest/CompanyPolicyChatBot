4fe2da8a6e798e2ed991d7ddc7333eeca536f9ee
rajanjasanicrest <159512935+rajanjasanicrest@users.noreply.github.com>
2024-03-29 10:50:10 +0530

    Update CompanyPolicyChatBot.py

--- CompanyPolicyChatBot.py
+++ CompanyPolicyChatBot.py
@@ -20,7 +20,7 @@ st.title('Policy Chatbot')
 
 # st.write(data)
 
-embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
+embeddings = OpenAIEmbeddings(model="text-embedding-3-small")
 vectorstore = FAISS.from_documents(data, embeddings)
 
 chain = ConversationalRetrievalChain.from_llm(
